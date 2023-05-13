77245eedb20b7f1ab81da29bbacd5961f51be0ef
Mukul Joshi <joshimukul29@gmail.com>
2023-05-13 18:41:26 +0530

    Using OpenAI instead of Llama and Gpt4

--- ingest.py
+++ ingest.py
@@ -1,18 +1,17 @@
 import os
 from dotenv import load_dotenv
 from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, TokenTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.embeddings import LlamaCppEmbeddings
+from langchain.embeddings import LlamaCppEmbeddings, OpenAIEmbeddings
 from constants import CHROMA_SETTINGS
 
 load_dotenv()
 
-def main():
-    llama_embeddings_model = os.environ.get('LLAMA_EMBEDDINGS_MODEL')
-    persist_directory = os.environ.get('PERSIST_DIRECTORY')
-    model_n_ctx = os.environ.get('MODEL_N_CTX')
-    # Load document and split in chunks
+def load_documents_from_memory():
+    loaders = []  # List to store loaders
+    documents = []  # List to store loaded documents
+
     for root, dirs, files in os.walk("source_documents"):
         for file in files:
             if file.endswith(".txt"):
@@ -21,13 +20,35 @@ def main():
                 loader = PDFMinerLoader(os.path.join(root, file))
             elif file.endswith(".csv"):
                 loader = CSVLoader(os.path.join(root, file))
-    documents = loader.load()
+
+            loaders.append(loader)  # Store the loader for each file
+
+    # Load documents using each loader
+    for loader in loaders:
+        loaded_docs = loader.load()
+        documents.extend(loaded_docs)  # Append the loaded documents to the list
+
+    return documents
+
+def main():
+
+    llama_embeddings_model = os.environ.get('LLAMA_EMBEDDINGS_MODEL')
+    persist_directory = os.environ.get('PERSIST_DIRECTORY')
+    model_n_ctx = os.environ.get('MODEL_N_CTX')
+    model_type = os.environ.get('MODEL_TYPE')
+    documents = load_documents_from_memory()
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     texts = text_splitter.split_documents(documents)
+    print(texts)
     # Create embeddings
-    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
+    # llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
+    match model_type:
+        case "OpenAI":
+            embedding_func = OpenAIEmbeddings()
+        case _default:
+            embedding_func = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
     # Create and store locally vectorstore
-    db = Chroma.from_documents(texts, llama, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
+    db = Chroma.from_documents(texts, embedding_func, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
     db.persist()
     db = None
 
--- privateGPT.py
+++ privateGPT.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
-from langchain.chains import RetrievalQA
-from langchain.embeddings import LlamaCppEmbeddings
+from langchain.chains import RetrievalQA, ConversationChain, ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import LlamaCppEmbeddings, OpenAIEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
-from langchain.llms import GPT4All, LlamaCpp
+from langchain.llms import OpenAI, LlamaCpp, GPT4All
 import os
 
 load_dotenv()
@@ -18,20 +19,32 @@ model_n_ctx = os.environ.get('MODEL_N_CTX')
 from constants import CHROMA_SETTINGS
 
 def main():
-    llama = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
-    db = Chroma(persist_directory=persist_directory, embedding_function=llama, client_settings=CHROMA_SETTINGS)
+    match model_type:
+        case "OpenAI":
+            embedding_func = OpenAIEmbeddings()
+        case _default:
+            embedding_func = LlamaCppEmbeddings(model_path=llama_embeddings_model, n_ctx=model_n_ctx)
+
+    db = Chroma(persist_directory=persist_directory, embedding_function=embedding_func, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever()
     # Prepare the LLM
     callbacks = [StreamingStdOutCallbackHandler()]
+    llm = None
     match model_type:
         case "LlamaCpp":
             llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, callbacks=callbacks, verbose=False)
         case "GPT4All":
             llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', callbacks=callbacks, verbose=False)
+        case 'OpenAI':
+            llm = OpenAI()
         case _default:
             print(f"Model {model_type} not supported!")
-            exit;
+            exit
+
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # qa = ConversationalRetrievalChain.from_llm(llm=OpenAI(temperature=0), retriever=retriever, memory=memory)
+
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
@@ -39,8 +52,12 @@ def main():
             break
         
         # Get the answer from the chain
-        res = qa(query)    
-        answer, docs = res['result'], res['source_documents']
+        res = qa(query)
+        answer = res['result']
+        # query = "What did the president say about Ketanji Brown Jackson"
+        # res = qa({"question": query})
+        # answer = res['answer']
+
 
         # Print the result
         print("\n\n> Question:")
@@ -49,9 +66,9 @@ def main():
         print(answer)
         
         # Print the relevant sources used for the answer
-        for document in docs:
-            print("\n> " + document.metadata["source"] + ":")
-            print(document.page_content)
+        # for document in docs:
+        #     print("\n> " + document.metadata["source"] + ":")
+        #     print(document.page_content)
 
 if __name__ == "__main__":
     main()
